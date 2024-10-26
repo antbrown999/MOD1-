0d250cefdb0acf83a15e6ee069772e26bec025c5
antbrown999 <164958480+antbrown999@users.noreply.github.com>
2024-10-26 08:06:42 -0500

    Add files via upload

---
+++ e4.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a -b; 
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function printHello(name) {
+    console.log('Hello' + name)
+}
+
+console.log(add(4,6)); // 10
+console.log(subtract(7,3)); // 4
+console.log(multiply(8,2)); // 16
+console.log(divide(6,2)); // 3
+printHello('name')
